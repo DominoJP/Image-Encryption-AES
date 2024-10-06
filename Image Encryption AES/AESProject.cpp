4f31e8ad14eddf950ad594def1d9778578de3771
andrew-miner <andrew.miner3@gmail.com>
2024-10-05 17:18:22 -0700

    Fully functioning block encryption.

--- Image Encryption AES/AESProject.cpp
+++ Image Encryption AES/AESProject.cpp
@@ -13,9 +13,14 @@
 #define AES_BLOCK_ROWS 4
 #define BUFFER_SIZE AES_BLOCK_SIZE
 #define KEY_SIZE 128
-#define KEY_CHAR_SIZE KEY_SIZE / (sizeof(char) * 8)
+#define KEY_CHAR_SIZE KEY_SIZE / (sizeof(unsigned char) * 8)
 
-char KEY[KEY_CHAR_SIZE] = {
+unsigned char KEY[KEY_CHAR_SIZE] = {
+    0x54, 0x68, 0x61, 0x74,
+    0x73, 0x20, 0x6D, 0x79,
+    0x20, 0x4B, 0x75, 0x6E,
+    0x67, 0x20, 0x46, 0x75
+    /*
     0x2b, 0x7e, 0x15, 0x16,
     0x28, 0xae, 0xd2, 0xa6,
     0xab, 0xf7, 0x15, 0x88,
@@ -41,7 +46,7 @@ char KEY[KEY_CHAR_SIZE] = {
 const int KEY_WORDS_SIZE = KEY_SIZE / (sizeof(uint32_t) * 8);
 uint32_t* KEY_WORDS = reinterpret_cast<uint32_t*>(KEY);
 
-char RCON[10][4] = {
+unsigned char RCON[10][4] = {
     {0x01, 0x00, 0x00, 0x00},
     {0x02, 0x00, 0x00, 0x00},
     {0x04, 0x00, 0x00, 0x00},
@@ -56,15 +61,19 @@ char RCON[10][4] = {
 
 uint32_t roundWords[11 * 4] = { 0 };
 
-void sBoxSubstitution(char* const& buffer, const int bufferSize);
+// Function Declarations
+bool testKnown();
+int testFunc(char* fileName);
+void sBoxSubstitution(unsigned char* const& buffer, const int bufferSize);
 void rotateWordsLeft(uint32_t& words, const int shiftAmount);
 void shiftCols(uint32_t* const& buffer, const int rowCount);
-void shiftRows(std::vector<char>& buffer, const int rowCount);
-void xorByteArray(std::vector<char>& buffer, char* key, int keySize);
-void print2DBuffer(const std::vector<char>& buffer, const int rowCount);
+void shiftRows(std::vector<unsigned char>& buffer, const int rowCount);
+void xorByteArray(unsigned char* buffer, unsigned char* key, int keySizeBytes);
+void print2DBuffer(const std::vector<unsigned char>& buffer, const int rowCount);
 void print2DBuffer(const unsigned char* const& buffer, const int size, const int rowCount);
-std::vector<char> mixColumns(std::vector<char>& buffer, const int rowCount);
+std::vector<unsigned char> mixColumns(std::vector<unsigned char>& buffer, const int rowCount);
 void expandKeys(uint32_t* const& roundWords, int numRounds, const uint32_t* const& key, int keySize);
+void encryptBlock(std::vector<unsigned char>& buffer, uint32_t* roundWords, const int numRounds, const uint32_t* const key, const int keySizeWords);
 
 int main(int argc, char* argv[])
 {
@@ -73,82 +82,72 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    std::vector<char> buffer = std::vector<char>(BUFFER_SIZE, 0);
+    std::vector<unsigned char> buffer = std::vector<unsigned char>(BUFFER_SIZE, 0);
 
     std::ifstream fin(argv[1], std::ifstream::binary);
     if (!fin.is_open()) {
-        std::cout << "Error: could not open file " << argv[0] << std::endl;
-        return -1;
+        std::cout << "Error: could not open file " << argv[1] << std::endl;
+        return 1;
     }
 
     std::cout << "Read File: " << argv[1] << std::endl;
 
+    //testFunc(argv[1]);
+
+    // Generate keys for each round
+    expandKeys(roundWords, 10, KEY_WORDS, KEY_WORDS_SIZE);
+
     //while (!fin.eof()) 
-    if (!fin.eof()) {
-        fin.read(buffer.data(), buffer.size());
+    if (false && !fin.eof()) {
+        fin.read(reinterpret_cast<char*>(buffer.data()), buffer.size());
         std::streamsize s = fin.gcount();
 
-
-        std::cout << "Bytes Read: ";
-        for (char c : buffer)
-            std::cout << std::bitset<8>(c) << " ";
-        std::cout << std::endl;
-
-        std::cout << "2D Print: " << std::endl;
+        std::cout << "Original Data: \n";
         print2DBuffer(buffer, AES_BLOCK_ROWS);
 
-        std::cout << "S-Subbed: \n";
-        sBoxSubstitution(buffer.data(), buffer.size());
+        encryptBlock(buffer, roundWords, 10, KEY_WORDS, KEY_WORDS_SIZE);
+
+        std::cout << "Encrypted Data: \n";
         print2DBuffer(buffer, AES_BLOCK_ROWS);
-
-        std::cout << "Shifted Rows1: \n";
-        shiftRows(buffer, AES_BLOCK_ROWS);
-        print2DBuffer(buffer, AES_BLOCK_ROWS);
-
-        std::cout << "Mixed Cols: \n" << std::endl;
-        buffer = mixColumns(buffer, AES_BLOCK_ROWS);
-        print2DBuffer(buffer, AES_BLOCK_ROWS);
-
-        std::cout << "Xor With: " << std::endl;
-        for (char c : KEY) {
-            std::cout << std::bitset<8>(c) << " ";
-        }
-        std::cout << std::endl;
-        xorByteArray(buffer, KEY, KEY_CHAR_SIZE);
-        print2DBuffer(buffer, AES_BLOCK_ROWS);
-
-        uint32_t* RCON_WORDS = reinterpret_cast<uint32_t*>(RCON);
-        char test[4] = { 0x09, 0xcf, 0xf4, 0x3c };
-        std::cout << std::endl;
-        for (char c : test) {
-            std::cout << static_cast<int>(c) << "\t";
-        }
-        std::cout << std::endl;
-        uint32_t* TEST_WORDS = reinterpret_cast<uint32_t*>(test);
-        rotateWordsLeft(*TEST_WORDS, 5);
-        std::cout << std::endl;
-        for (char c : test) {
-            std::cout << static_cast<int>(c) << "\t";
-        }
-        std::cout << std::endl;
-
-        std::cout << "Key: " << std::endl;
-        print2DBuffer(reinterpret_cast<unsigned char*>(KEY), KEY_CHAR_SIZE, 4);
-        std::cout << "Round Keys: " << std::endl;
-        expandKeys(roundWords, 10, KEY_WORDS, KEY_WORDS_SIZE);
-        print2DBuffer(reinterpret_cast<unsigned char*>(roundWords), 176, 16);
-
-        std::cout << std::dec << std::bitset<8>(0x57) << std::endl;
-        std::cout << std::dec << std::bitset<8>(0x57 << 1) << std::endl;
-
     }
 
     // Done and close.
     fin.close();
 
+    testKnown();
+
     return 0;
 }
 
+
+void encryptBlock(std::vector<unsigned char> &buffer, uint32_t* roundWords, const int numRounds, const uint32_t* const key, const int keySizeWords)
+{
+    // Ensure buffer size is 16 bytes (128 bits)
+    assert(buffer.size() == 16);
+
+    uint32_t* roundKey = roundWords;
+
+    xorByteArray(buffer.data(), reinterpret_cast<unsigned char*>(roundKey), 16);
+
+    for (int r = 0; r < numRounds - 1; ++r) {
+        sBoxSubstitution(buffer.data(), buffer.size());
+        shiftRows(buffer, AES_BLOCK_ROWS);
+        buffer = mixColumns(buffer, AES_BLOCK_ROWS);
+        
+        // Increment to next roundKey
+        // Must add 4 because each round key is 128 bits
+        roundKey += 4; // 4 * 32-bit words = 16 bytes = 128 bits
+        xorByteArray(buffer.data(), reinterpret_cast<unsigned char*>(roundKey), 16);
+    }
+
+    sBoxSubstitution(buffer.data(), buffer.size());
+    shiftRows(buffer, AES_BLOCK_ROWS);
+
+    // Increment to next roundKey
+    // Must add 4 because each round key is 128 bits
+    roundKey += 4; // 4 * 32-bit words = 16 bytes = 128 bits
+    xorByteArray(buffer.data(), reinterpret_cast<unsigned char*>(roundKey), 16);
+}
 void expandKeys(uint32_t* const& roundWords, int numRounds, const uint32_t* const& key, int keySize)
 {
     int i = 0;
@@ -160,19 +159,14 @@ void expandKeys(uint32_t* const& roundWords, int numRounds, const uint32_t* cons
         uint32_t temp = roundWords[i - 1];
         if (i % keySize == 0) {
             rotateWordsLeft(temp, 1);
-            sBoxSubstitution(reinterpret_cast<char*>(&temp), 4);
+            sBoxSubstitution(reinterpret_cast<unsigned char*>(&temp), 4);
             temp = temp ^ *reinterpret_cast<uint32_t*>(RCON[i / keySize - 1]);
         }
         else if (keySize > 6 && i % keySize == 4) {
-            sBoxSubstitution(reinterpret_cast<char*>(&temp), 4);
+            sBoxSubstitution(reinterpret_cast<unsigned char*>(&temp), 4);
         }
         roundWords[i] = roundWords[i - keySize] ^ temp;
     }
-}
-
-void encryptBlock(char* const buffer, const int& key)
-{
-
 }
 
 void rotateWordsLeft(uint32_t& words, const int shiftAmount)
@@ -186,23 +180,30 @@ void rotateWordsLeft(uint32_t& words, const int shiftAmount)
     words = shiftedRight | shiftedLeft;
 }
 
-void xorByteArray(std::vector<char>& buffer, char* key, int keySize)
+void xorByteArray(unsigned char* buffer, unsigned char* key, int keySizeBytes)
 {
-    assert(keySize % sizeof(uint64_t) == 0);
-    std::cout << "Key Size: " << keySize << " int64: " << sizeof(uint64_t) << std::endl;
+    assert(keySizeBytes % sizeof(uint64_t) == 0);
 
     // Xor the buffer in as few iterations as possible
-    uint64_t* buffer64 = reinterpret_cast<uint64_t*>(buffer.data());
+    uint64_t* buffer64 = reinterpret_cast<uint64_t*>(buffer);
     uint64_t* key64 = reinterpret_cast<uint64_t*>(key);
 
-    for (int i = 0; i < (keySize / sizeof(uint64_t)); ++i) {
+    for (int i = 0; i < (keySizeBytes / sizeof(uint64_t)); ++i) {
         *(buffer64 + i) = *(buffer64 + i) ^ *(key64 + i);
     }
 }
 
-std::vector<char> mixColumns(std::vector<char>& buffer, const int rowCount)
+unsigned char galoisMultiplyBy2(unsigned char value) {
+    unsigned char result = value << 1;
+    if (value & 0x80) { // If the most significant bit is set (overflow)
+        result ^= 0x1b; // XOR with the AES irreducible polynomial
+    }
+    return result;
+}
+
+std::vector<unsigned char> mixColumns(std::vector<unsigned char>& buffer, const int rowCount)
 {
-    static const char COL_MIXER[AES_BLOCK_COLS][AES_BLOCK_ROWS] = {
+    static const unsigned char COL_MIXER[AES_BLOCK_COLS][AES_BLOCK_ROWS] = {
         {0x02, 0x03, 0x01, 0x01},
         {0x01, 0x02, 0x03, 0x01},
         {0x01, 0x01, 0x02, 0x03},
@@ -211,33 +212,34 @@ std::vector<char> mixColumns(std::vector<char>& buffer, const int rowCount)
 
     assert(buffer.size() % rowCount == 0);
 
-    std::vector<char> mixed = std::vector<char>(BUFFER_SIZE, 0);
-    char temp = 0;
+    std::vector<unsigned char> mixed = std::vector<unsigned char>(BUFFER_SIZE, 0);
     int colCount = buffer.size() / rowCount;
-    for (int col1 = 0; col1 < colCount; ++col1) {
-        for (int row2 = 0; row2 < rowCount; ++row2) {
-            for (int col12 = 0; col12 < rowCount; ++col12) {
-                switch (COL_MIXER[row2][col12]) {
+    for (int col = 0; col < colCount; ++col) {
+
+        for (int mixerRow = 0; mixerRow < AES_BLOCK_ROWS; ++mixerRow) {
+            unsigned char mixedValue = 0;  // Temporary value to accumulate results
+            for (int mixerCol = 0; mixerCol < AES_BLOCK_COLS; ++mixerCol) {
+                unsigned char temp = 0;
+                unsigned char value = buffer[col * rowCount + mixerCol];
+                switch (COL_MIXER[mixerRow][mixerCol]) {
                     case 1:
-                        mixed[col1 * rowCount + row2] ^= buffer[col1 * rowCount + col12];
+                        temp = value;
                         break;
                     case 2:
-                        temp = buffer[col1 * rowCount + col12] << 1;
-                        if (buffer[col1 * rowCount + col12] > 0x80)
-                            temp ^= 0x1b;
-                        mixed[col1 * rowCount + row2] ^= temp;
+                        temp = galoisMultiplyBy2(value);
                         break;
                     case 3:
-                        temp = buffer[col1 * rowCount + col12] << 1;
-                        if (buffer[col1 * rowCount + col12] > 0x80)
-                            temp ^= 0x1b;
-                        mixed[col1 * rowCount + row2] ^= temp ^ buffer[col1 * rowCount + col12];
+                        temp = galoisMultiplyBy2(value) ^ value;
                         break;
                     default:
                         std::cout << "Error: Invalid Constant Array!" << std::endl;
+                        return std::vector<unsigned char>();
                 }
+                mixedValue ^= temp;
             }
+            mixed[col * rowCount + mixerRow] = mixedValue;
         }
+
     }
 
     return mixed;
@@ -250,7 +252,7 @@ void shiftCols(uint32_t* const& buffer, const int rowCount)
     }
 }
 
-void shiftRows(std::vector<char>& buffer, const int rowCount)
+void shiftRows(std::vector<unsigned char>& buffer, const int rowCount)
 {
     assert(buffer.size() % rowCount == 0);
 
@@ -259,7 +261,7 @@ void shiftRows(std::vector<char>& buffer, const int rowCount)
         int shift = row;
 
         // Max of 3 temps with 4x4 blocks
-        std::vector<char> temps = std::vector<char>(AES_BLOCK_COLS - 1, 0);
+        std::vector<unsigned char> temps = std::vector<unsigned char>(AES_BLOCK_COLS - 1, 0);
 
         // Copy Temp Values
         for (int col = 0; col < shift; ++col)
@@ -278,9 +280,9 @@ void shiftRows(std::vector<char>& buffer, const int rowCount)
     }
 }
 
-void sBoxSubstitution(char* const& buffer, const int bufferSize)
+void sBoxSubstitution(unsigned char* const& buffer, const int bufferSize)
 {
-    static const char sBox[AES_BLOCK_SIZE][AES_BLOCK_SIZE] = {
+    static const unsigned char sBox[AES_BLOCK_SIZE][AES_BLOCK_SIZE] = {
         {0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76} ,
         {0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0},
         {0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15},
@@ -310,29 +312,160 @@ void sBoxSubstitution(char* const& buffer, const int bufferSize)
     }
 }
 
-void print2DBuffer(const std::vector<char>& buffer, const int rowCount)
+void print2DBuffer(const std::vector<unsigned char>& buffer, const int rowCount)
 {
     assert(buffer.size() % rowCount == 0);
 
     int colCount = buffer.size() / rowCount;
     for (int row = 0; row < rowCount; ++row) {
         for (int col = 0; col < colCount; ++col) {
-            std::cout << static_cast<int>(buffer.at(col * rowCount + row)) << "\t";
+            std::cout << std::hex << std::bitset<8>(buffer.at(col * rowCount + row)).to_ulong() << "\t";
+            //std::cout << std::hex << static_cast<unsigned>(buffer[col * colCount + row]) << "\t";
         }
         std::cout << std::endl;
     }
 }
 
 
-void print2DBuffer(const unsigned char* const& buffer, const int size, const int rowCount)
+void print2DBuffer(const unsigned char* const& buffer, const int size, const int colCount)
 {
-    assert(size % rowCount == 0);
+    assert(size % colCount == 0);
 
-    int colCount = size / rowCount;
-    for (int col = 0; col < colCount; ++col) {
-        for (int row = 0; row < rowCount; ++row) {
-            std::cout << std::hex << static_cast<unsigned>(buffer[col * rowCount + row]) << "\t";
+    int rowCount = size / colCount;
+    for (int col = 0; col < rowCount; ++col) {
+        for (int row = 0; row < colCount; ++row) {
+            std::cout << std::hex << static_cast<unsigned>(buffer[col * colCount + row]) << "\t";
         }
         std::cout << std::endl;
     }
+}
+
+bool testKnown()
+{
+    static const unsigned char KNOWN_KEY[KEY_CHAR_SIZE] = {
+        0x54, 0x68, 0x61, 0x74,
+        0x73, 0x20, 0x6D, 0x79,
+        0x20, 0x4B, 0x75, 0x6E,
+        0x67, 0x20, 0x46, 0x75
+    };
+
+    const int KNOWN_KEY_WORDS_SIZE = KEY_SIZE / (sizeof(uint32_t) * 8);
+    const uint32_t* KNOWN_KEY_WORDS = reinterpret_cast<const uint32_t*>(KNOWN_KEY);
+
+    static const unsigned char EXPECTED_ENCRYPTION[KEY_CHAR_SIZE] = {
+        0x29, 0xC3, 0x50, 0x5F,
+        0x57, 0x14, 0x20, 0xF6,
+        0x40, 0x22, 0x99, 0xB3,
+        0x1A, 0x02, 0xD7, 0x3A
+    };
+
+    std::vector<unsigned char> dataBuffer = {
+        0x54, 0x77, 0x6F, 0x20,
+        0x4F, 0x6E, 0x65, 0x20,
+        0x4E, 0x69, 0x6E, 0x65,
+        0x20, 0x54, 0x77, 0x6F
+    };
+
+    uint32_t roundWords[11 * 4] = { 0 };
+    expandKeys(roundWords, 10, KNOWN_KEY_WORDS, KNOWN_KEY_WORDS_SIZE);
+
+    std::cout << "\nKey: \n";
+    print2DBuffer(KNOWN_KEY, KEY_CHAR_SIZE, 16);
+
+    std::cout << "\nOriginal Data: \n";
+    print2DBuffer(dataBuffer.data(), dataBuffer.size(), 16);
+
+    std::cout << "\nExpected Encryption: \n";
+    print2DBuffer(EXPECTED_ENCRYPTION, KEY_CHAR_SIZE, 16);
+
+    encryptBlock(dataBuffer, roundWords, 10, KNOWN_KEY_WORDS, KNOWN_KEY_WORDS_SIZE);
+
+    std::cout << "\nEncrypted Data: \n";
+    print2DBuffer(dataBuffer.data(), dataBuffer.size(), 16);
+    
+    bool matched = true;
+    for (int i = 0; i < KEY_CHAR_SIZE; ++i) {
+        if (dataBuffer[i] != EXPECTED_ENCRYPTION[i]) {
+            matched = false;
+            break;
+        }
+    }
+    std::cout << "\nExpected Encryption and Actual Encryption Match: " << (matched ? "true" : "false") << std::endl;
+
+    return matched;
+}
+
+int testFunc(char* fileName)
+{
+    std::vector<unsigned char> buffer = std::vector<unsigned char>(BUFFER_SIZE, 0);
+
+    std::ifstream fin(fileName, std::ifstream::binary);
+    if (!fin.is_open()) {
+        std::cout << "Error: could not open file " << fileName << std::endl;
+        return 1;
+    }
+
+    std::cout << "Read File: " << fileName << std::endl;
+
+    if (!fin.eof()) {
+        fin.read(reinterpret_cast<char*>(buffer.data()), buffer.size());
+        std::streamsize s = fin.gcount();
+
+
+        std::cout << "Bytes Read: ";
+        for (unsigned char c : buffer)
+            std::cout << std::bitset<8>(c) << " ";
+        std::cout << std::endl;
+
+        std::cout << "2D Print: " << std::endl;
+        print2DBuffer(buffer, AES_BLOCK_ROWS);
+
+        std::cout << "S-Subbed: \n";
+        sBoxSubstitution(buffer.data(), buffer.size());
+        print2DBuffer(buffer, AES_BLOCK_ROWS);
+
+        std::cout << "Shifted Rows1: \n";
+        shiftRows(buffer, AES_BLOCK_ROWS);
+        print2DBuffer(buffer, AES_BLOCK_ROWS);
+
+        std::cout << "Mixed Cols: \n" << std::endl;
+        buffer = mixColumns(buffer, AES_BLOCK_ROWS);
+        print2DBuffer(buffer, AES_BLOCK_ROWS);
+
+        std::cout << "Xor With: " << std::endl;
+        for (unsigned char c : KEY) {
+            std::cout << std::bitset<8>(c) << " ";
+        }
+        std::cout << std::endl;
+        xorByteArray(buffer.data(), KEY, KEY_CHAR_SIZE);
+        print2DBuffer(buffer, AES_BLOCK_ROWS);
+
+        uint32_t* RCON_WORDS = reinterpret_cast<uint32_t*>(RCON);
+        unsigned char test[4] = { 0x09, 0xcf, 0xf4, 0x3c };
+        std::cout << std::endl;
+        for (unsigned char c : test) {
+            std::cout << static_cast<int>(c) << "\t";
+        }
+        std::cout << std::endl;
+        uint32_t* TEST_WORDS = reinterpret_cast<uint32_t*>(test);
+        rotateWordsLeft(*TEST_WORDS, 5);
+        std::cout << std::endl;
+        for (unsigned char c : test) {
+            std::cout << static_cast<int>(c) << "\t";
+        }
+        std::cout << std::endl;
+
+        std::cout << "Key: " << std::endl;
+        print2DBuffer(KEY, KEY_CHAR_SIZE, 4);
+        std::cout << "Round Keys: " << std::endl;
+        expandKeys(roundWords, 10, KEY_WORDS, KEY_WORDS_SIZE);
+        print2DBuffer(reinterpret_cast<unsigned char*>(roundWords), 176, 16);
+
+        std::cout << std::dec << std::bitset<8>(0x57) << std::endl;
+        std::cout << std::dec << std::bitset<8>(0x57 << 1) << std::endl;
+
+    }
+
+    fin.close();
+    return 0;
 }
